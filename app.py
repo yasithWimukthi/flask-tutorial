5e51d4dbbd7ef9bee0f3a080886a8c34150c699f
yasith <it19966922@my.sliit.lk>
2022-08-31 18:37:03 +0530

    create routes

--- app.py
+++ app.py
@@ -3,10 +3,29 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/store')
+def get_stores():
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
+
+app.run(port=5000)

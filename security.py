19eb6225b2778d962c9e31da47b775f6e30fcd49
yasith <it19966922@my.sliit.lk>
2022-09-01 20:57:52 +0530

    implement authentication

---
+++ security.py
@@ -0,0 +1,23 @@
+users = [
+    {
+        'id': 1,
+        'name': 'bob',
+        'password': 'asdf'
+    }
+]
+
+username_mapping = {
+    'bob': {
+        'id': 1,
+        'name': 'bob',
+        'password': 'asdf'
+    }
+}
+
+userid_mapping = {
+    1: {
+        'id': 1,
+        'name': 'bob',
+        'password': 'asdf'
+    }
+}

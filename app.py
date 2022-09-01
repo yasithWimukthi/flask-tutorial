2787e3b2a0697ea1bd36b19605c9df30a03b0392
yasith <it19966922@my.sliit.lk>
2022-09-01 09:12:41 +0530

    create routes

--- app.py
+++ app.py
@@ -1,31 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-
-@app.route('/store', methods=['POST'])
-def create_store():
-    pass
-
-
-@app.route('/store/<string:name>')
-def get_store(name):
-    pass
-
-
-@app.route('/store')
-def get_stores():
-    pass
-
-
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    pass
-
-
-@app.route('/store/<string:name>/item')
-def get_item_in_store(name):
-    pass
 
 
 app.run(port=5000)

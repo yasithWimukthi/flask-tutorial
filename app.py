1afd14f13da33961babce5b695f727ba3b82fc94
yasith <it19966922@my.sliit.lk>
2022-09-01 09:13:57 +0530

    create routes

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item, 201
+
+
+api.add_resource(Item, '/item/<string:name>')
 
 app.run(port=5000)

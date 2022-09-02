ded270409d24b57c8f3ded2a0a4606733c9239ff
yasith <it19966922@my.sliit.lk>
2022-09-02 21:44:36 +0530

    implement update items method

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help='This field cannot be left blank')
+
     @jwt_required()
     def get(self, name):
         # for item in items:
@@ -26,7 +29,7 @@ class Item(Resource):
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -37,9 +40,7 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price', type=float, required=True, help='This field cannot be left blank')
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}

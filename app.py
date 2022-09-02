972075d63568f149163fc355c92284ed5f30bfd7
yasith <it19966922@my.sliit.lk>
2022-09-02 21:32:44 +0530

    implement update items method

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from  flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -37,7 +37,9 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument('price', type=float, required=True, help='This field cannot be left blank')
+        data = parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}

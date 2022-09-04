30613e197d7eadb995b716e1f9aad7b1513efae0
yasith <it19966922@my.sliit.lk>
2022-09-04 10:58:45 +0530

    create database

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
@@ -6,3 +9,18 @@ class User:
 
     def __str__(self):
         return "User: " + self.username + " Password: " + self.password
+
+    def find_by_username(self, username):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM users WHERE username=?"
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        if row:
+            user = User(row[0], row[1], row[2])
+        else:
+            user = None
+
+        connection.close()
+        return user

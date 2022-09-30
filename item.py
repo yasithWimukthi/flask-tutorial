f232341b75ef7df63cef700ea9799a9fba4e3d45
yasith <it19966922@my.sliit.lk>
2022-09-30 23:47:14 +0530

    implement insert method

--- item.py
+++ item.py
@@ -28,13 +28,17 @@ class Item(Resource):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
         data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
+
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         query = "INSERT INTO items VALUES (?, ?)"
         cursor.execute(query, (item['name'], item['price']))
         connection.commit()
         connection.close()
-        return item, 201
 
     def delete(self, name):
         connection = sqlite3.connect('data.db')
